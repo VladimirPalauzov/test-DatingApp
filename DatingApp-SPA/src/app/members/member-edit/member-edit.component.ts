795cf12345731f1ec5b7378437a3da35831e6e3e
VladimirPalauzov <vladogp@yahoo.com>
2018-11-09 16:48:12 +0200

    End of section 10

---
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
+import { User } from 'src/app/_models/user';
+import { ActivatedRoute } from '@angular/router';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from 'src/app/_services/auth.service';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm;
+  user: User;
+
+  // we use this because Angular router cannot guard route deactivation if user try to close the browser
+  @HostListener('window:beforeunload', ['$event']) unloadNotification($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
+
+  constructor(private route: ActivatedRoute, private alretify: AlertifyService,
+    private userService: UserService, private authService: AuthService) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.user = data['user'];
+    });
+  }
+
+  updateUser() {
+    this.userService.updateUser(this.authService.decodedToken.nameid, this.user)
+      .subscribe(next => {
+        this.alretify.success('Profile updated successfully');
+        this.editForm.reset(this.user);
+      }, error => {
+        this.alretify.error(error);
+      });
+  }
+}
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -8,6 +8,9 @@ import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 /*
 export const appRoutes: Routes = [
@@ -31,6 +34,9 @@ export const appRoutes: Routes = [
                 resolve: {users: MemberListResolver}},
             { path: 'members/:id', component: MemberDetailComponent,
                 resolve: {user: MemberDetailResolver}},
+            { path: 'member/edit', component: MemberEditComponent,
+                resolve: {user: MemberEditResolver},
+                canDeactivate: [PreventUnsavedChanges]}, // no need for user id in the path, we will take it from the user jwt
             { path: 'messages', component: MessagesComponent},
             { path: 'lists', component: ListsComponent}
         ]
